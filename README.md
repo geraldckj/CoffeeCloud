ffcf943a2a95148fca4ad0a823593474205bce82
Gerald <geraldckj13@gmail.com>
2020-08-16 18:58:34 +0800

    Commit 1.0
    Signed-off-by: Gerald <geraldckj13@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # CoffeeCloud
 CoffeeCloud with Django
 <br>
-Trial Edit 1
+Run program with: `python manage.py runserver`
+
+To update db: <br>
+`python manage.py makemigrations`<br>
+`python manage.py migrate`<br>
+You can update db structure post creation with migrations. <br>
+
+To run SQL commands:
+ <br>
+`python manage.py sqlmigrate coffeeCloud 0001`
+<br>*0001 is migration number. It's shown when `makemigrations` command is passed.
--- venv/django_coffeesite/coffeeCloud/models.py
+++ venv/django_coffeesite/coffeeCloud/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class beans(models.Model):
+    name = models.CharField(max_length=100)
+    region = models.CharField(max_length=100)
+    roast = models.CharField(max_length=100)
+    description = models.TextField()
+    dateRoast = models.DateTimeField(default=timezone.now)
+
+#reviews that users can leave behind for beans, merch & accessories
+#if diff classes of products are introduced, might need to add new review tables for each
+class Reviews(models.Model):
+    #relationship fields
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(beans, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=100)
+    review = models.TextField()
+    datePosted = models.DateTimeField(default=timezone.now)
+
