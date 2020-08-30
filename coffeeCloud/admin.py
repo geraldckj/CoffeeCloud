66bde15b7673ee7948438e1feffa89dcda484a58
Gerald <geraldckj13@gmail.com>
2020-08-30 23:15:34 +0800

    'Reset'

--- coffeeCloud/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from coffeeCloud.models import Reviews, Beans, DailyLog
-
-admin.site.register(Reviews)
-admin.site.register(Beans)
-admin.site.register(DailyLog)
--- coffeeCloud/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class coffeeCloudConfig(AppConfig):
-    name = 'coffeeCloud'
